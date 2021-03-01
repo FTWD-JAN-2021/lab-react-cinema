9908ec04dcb26ce627b47350bce61a6cb4055571
Rastko <rastkodjurica@yahoo.com>
2021-03-01 11:06:16 -0500

    Rastko

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -3,6 +3,7 @@ import Home from './components/Home';
 
 class App extends Component {
   render() {
+    console.log('im in the frontend!')
     return (
       <div className='App'>
         <Home />
